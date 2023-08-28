65fae40e94e65d83bf76eeff692a1601abf74937
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-28 12:33:54 +0300

    remove empty line

--- src/redux/store/infoWetherReducer.js
+++ src/redux/store/infoWetherReducer.js
@@ -2,9 +2,6 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 
 import { FETCH_INFO_ABOUT_CUR_PLACE, TAKE_CORDINATES } from "./actions";
 
-
-
-
 export const fetchWetherInfo = createAction(FETCH_INFO_ABOUT_CUR_PLACE)
 export const takeCordinates = createAction(TAKE_CORDINATES)
 
@@ -20,16 +17,11 @@ export const infoWetherReducer = createReducer(defaultState, {
     [fetchWetherInfo]: function (state, { payload }) {
         console.log(payload)
         state.curentWetherInfo.info = payload;
-        
+
     },
     [takeCordinates]: function (state, { payload }) {
         console.log(payload)
-        
-
-
         state.curCordinates.push(payload)
-
-
     },
 
 })
