6d7f84df8a1f4d70b648a4dcb449994ce9cb0a06
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-28 12:08:14 +0300

    createInfoReducer

---
+++ src/redux/store/infoWetherReducer.js
@@ -0,0 +1,35 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+import { FETCH_INFO_ABOUT_CUR_PLACE, TAKE_CORDINATES } from "./actions";
+
+
+
+
+export const fetchWetherInfo = createAction(FETCH_INFO_ABOUT_CUR_PLACE)
+export const takeCordinates = createAction(TAKE_CORDINATES)
+
+
+const defaultState = {
+    curentWetherInfo: {},
+    curCordinates: [],
+
+}
+//Reducer 
+export const infoWetherReducer = createReducer(defaultState, {
+
+    [fetchWetherInfo]: function (state, { payload }) {
+        console.log(payload)
+        state.curentWetherInfo.info = payload;
+        
+    },
+    [takeCordinates]: function (state, { payload }) {
+        console.log(payload)
+        
+
+
+        state.curCordinates.push(payload)
+
+
+    },
+
+})
